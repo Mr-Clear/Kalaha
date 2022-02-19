5ede2edc72f574122a4ff9f925fd4a7a69a6a1a4
Thomas <git@thomas.klierlinge.de>
2022-02-19 19:50:29 +0100

    Measure run time of Competition

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Player/RandomPlayer.h"
 #include "Player/SmartPlayer.h"
 
+#include <chrono>
 #include <memory>
 
 int main()
@@ -17,7 +18,12 @@ int main()
     std::shared_ptr<AbstractPlayer> playerB{std::make_shared<SmartPlayer>(PlayerNumber::Two)};
     std::map<PlayerNumber, std::shared_ptr<AbstractPlayer>> playerMap{{PlayerNumber::One, playerA}, {PlayerNumber::Two, playerB}};
     Competition c{{6, 6}, playerA, playerB, 10000, nullOutput};
+
+    const auto startTime = std::chrono::high_resolution_clock::now();
     auto outcome = c.run();
+    const auto endTime = std::chrono::high_resolution_clock::now();
     ConsoleOutput{}.showCompetitionEnd(outcome);
+    const double duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    std::cout << "Duration: " << duration << " ms" << std::endl;
     return 0;
 }
